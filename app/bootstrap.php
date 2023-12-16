14260f2fa8771c07aa5684e05e4376686eff30b0
zinebMachrouh <zinebmac49@gmail.com>
2023-12-17 00:27:35 +0100

    configuring the libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,11 +1,6 @@
 <?php
 
 require 'config/config.php';
-//Libraries
-
-// require 'libraries/Controller.php';
-// require 'libraries/Core.php';
-// require 'libraries/Database.php';
 
 spl_autoload_register(function ($className) {
     require 'libraries/' . $className . '.php';
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,23 +1,17 @@
 <?php
 
-// class Pages extends Controller
-// {
-//     private $userModel;
-//     public function __construct()
-//     {
-//     }
+class Pages extends Controller
+{
+    private $userModel;
+    public function __construct()
+    {
+    }
 
-//     public function index()
-//     {
-//         $data = [
-//             'title' => 'Hey'
-//         ];
-//         $this->view('pages/index', $data);
-//     }
-// }
-
-class Pages{
-    public function __construct() {
-        echo 'page loaded';
+    public function index()
+    {
+        $data = [
+            'title' => 'Hey'
+        ];
+        $this->view('pages/index', $data);
     }
 }
